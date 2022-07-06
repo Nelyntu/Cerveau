8743003a9482fa01cc3b515505ff26d836a4abda
Nelyntu <le.nelyntu@gmail.com>
2022-07-06 19:53:37 +0200

    remove "caracter cleaner" and format Commands code

--- Twitch/Commands.php
+++ Twitch/Commands.php
@@ -11,20 +11,20 @@ namespace Twitch;
 use Twitch\Command\CommandInterface;
 
 /**
- * Provides an easy way to have triggerable commands.
+ * Provides an easy way to handle commands.
  */
 class Commands
 {
-	protected Twitch $twitch;
-	protected int $logLevel;
+    protected Twitch $twitch;
+    protected int $logLevel;
     /** @var CommandInterface[] */
     protected array $commands = [];
 
-	public function __construct(Twitch $twitch, int $logLevel)
-	{
-		$this->twitch = $twitch;
-		$this->logLevel = $logLevel;
-	}
+    public function __construct(Twitch $twitch, int $logLevel)
+    {
+        $this->twitch = $twitch;
+        $this->logLevel = $logLevel;
+    }
 
     public function addCommand(CommandInterface $command): void
     {
@@ -34,22 +34,13 @@ class Commands
     public function handle(string $command, ?array $args = []): ?string
     {
         $this->twitch->emit("[HANDLE COMMAND] `$command`", Twitch::LOG_INFO);
-        $this->twitch->emit("[ARGS] ".print_r($args, true), Twitch::LOG_INFO);
-
-		if($this->logLevel === Twitch::LOG_DEBUG) {
-		$i = 0;
-		foreach ($args as $arg) {
-			$args[$i] = preg_replace('/[^A-Za-z0-9\-]/', '', trim($arg));
-			$i++;
-		}
-		unset($i);
-		}
+        $this->twitch->emit("[ARGS] " . print_r($args, true), Twitch::LOG_INFO);
 
         $response = null;
         $found = false;
 
-        foreach($this->commands as $commandHandler) {
-            if(!$commandHandler->supports($command)) {
+        foreach ($this->commands as $commandHandler) {
+            if (!$commandHandler->supports($command)) {
                 continue;
             }
             $found = true;
@@ -57,7 +48,7 @@ class Commands
         }
 
         if (!$found) {
-            $this->twitch->emit("[HANDLE COMMAND] `$command` NOT FOUND", Twitch::LOG_INFO);
+            $this->twitch->emit("[HANDLE COMMAND] `$command` NOT HANDLED", Twitch::LOG_INFO);
         }
 
         return $response;
