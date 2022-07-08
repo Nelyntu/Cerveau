c8c8c07916baf440de8de1038480d5a9e8a6b94d
Nelyntu <le.nelyntu@gmail.com>
2022-07-08 16:39:33 +0200

    change FQCN usage

--- run.php
+++ run.php
@@ -1,28 +1,25 @@
 <?php
-/**
- * This file is a part of the TwitchPHP project.
- *
- * Copyright (c) 2021 ValZarGaming <valzargaming@gmail.com>
- */
 
-use Twitch\CommandDispatcher;
+/**
+ * Original development by ValZarGaming <valzargaming@gmail.com>
+ */
 
 require 'vendor/autoload.php';
 
 $options = require __DIR__ . '/settings.php';
 
-$logger = new \Nelyntu\Logger($options['logLevel']);
-$userList = new \Twitch\UserList($options['nick'], $options['whitelist']);
-$commandDispatcher = new CommandDispatcher($options['commandsymbol'] ?? ['!'], $logger);
+$logger = new Nelyntu\Logger($options['logLevel']);
+$userList = new Twitch\UserList($options['nick'], $options['whitelist']);
+$commandDispatcher = new Twitch\CommandDispatcher($options['commandsymbol'] ?? ['!'], $logger);
 $twitch = new Twitch\Twitch($logger, $options, $commandDispatcher);
 
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\BanCommandHandlerHandler($twitch, $userList));
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\HelpCommandHandler($commandDispatcher));
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\JoinCommandHandler($twitch, $userList));
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\LeaveCommandHandler($twitch, $userList));
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\PhpCommandHandler($twitch, $userList));
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\SoCommandHandler($twitch, $userList));
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\StopCommandHandler($twitch, $userList));
-$commandDispatcher->addCommand(new \Twitch\CommandHandler\ResponseCommandHandler($twitch, $options['responses']));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\BanCommandHandlerHandler($twitch, $userList));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\HelpCommandHandler($commandDispatcher));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\JoinCommandHandler($twitch, $userList));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\LeaveCommandHandler($twitch, $userList));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\PhpCommandHandler($twitch, $userList));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\SoCommandHandler($twitch, $userList));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\StopCommandHandler($twitch, $userList));
+$commandDispatcher->addCommand(new Twitch\CommandHandler\ResponseCommandHandler($twitch, $options['responses']));
 
 $twitch->run();
