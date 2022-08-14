41f9faa07167d20c58816dcd0d746bf13708885c
Nelyntu <le.nelyntu@gmail.com>
2022-08-14 08:37:04 +0200

    command to reset hangman

---
+++ src/Twitch/CommandHandler/Hangman/HangmanResetCommandHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Twitch\CommandHandler\Hangman;
+
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Twitch\Command;
+use Twitch\CommandHandler\CommandHandlerInterface;
+use Twitch\UserList;
+
+class HangmanResetCommandHandler implements CommandHandlerInterface
+{
+    private const COMMAND_NAME = 'hg_reset';
+
+    public function __construct(private readonly UserList $userList, private readonly FilesystemAdapter $cache)
+    {
+    }
+
+    public function supports(string $name): bool
+    {
+        return $name === self::COMMAND_NAME;
+    }
+
+    public function handle(Command $command): ?string
+    {
+        $this->cache->deleteItem(HangmanCommandHandler::DATA_CACHE_KEY);
+
+        return 'ok';
+    }
+
+    public function isAuthorized(string $username): bool
+    {
+        return $username === $this->userList->streamer;
+    }
+
+    public function getName(): array
+    {
+        return [self::COMMAND_NAME];
+    }
+}
