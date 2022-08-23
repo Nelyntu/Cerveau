cf21a92ca60af740a01849c25312428bbcf67bbb
Nelyntu <le.nelyntu@gmail.com>
2022-08-23 14:35:44 +0200

    remove comments

--- src/App/Command/BotRunCommand.php
+++ src/App/Command/BotRunCommand.php
@@ -21,23 +21,8 @@ class BotRunCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // ... put here the code to create the user
-
         $this->twitch->run();
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
-
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
     }
 }
